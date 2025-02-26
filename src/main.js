9ffbbd55e94d17536f52faaa2f0ba3b66bfcf4c8
DieguitoNC <diegocatalann@gmail.com>
2025-02-26 10:41:23 -0300

    recomeco_projeto

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import "./modules/button/start-button.js"
+
+
---
+++ src/modules/button/start-button.js
@@ -0,0 +1,10 @@
+// Fazer algo quando alguem clicar no botao start
+
+
+//Seleciona a classe do botao iniciar 
+const startButton = document.querySelector(".start-button")
+
+// Aqui 
+const clickStart = startButton.addEventListener("click", function () {
+    console.log("Botao de Comecar foi acionaddo !")
+})
