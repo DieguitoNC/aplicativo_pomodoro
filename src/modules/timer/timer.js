b242d9e9ec62f97b3d6a698d71b55913fab32f0f
DieguitoNC <diegocatalann@gmail.com>
2025-03-01 17:25:11 -0300

    logica do cronometro funcionando

--- src/modules/timer/timer.js
+++ src/modules/timer/timer.js
@@ -1,2 +1,42 @@
 // Fazer toda a regra do cronometro !
 
+
+let minutes = 60
+
+function transformToSeconds(number) {
+    let seconds = minutes * 60
+    return (seconds)
+}
+
+
+function stopwatch(minutes) {
+    let seconds = transformToSeconds(minutes)
+    let min = minutes
+
+    while (seconds > 0) {
+        let minTimer = Math.floor(seconds / 60)
+        let secondsAtTimer = seconds % 60
+
+        
+        seconds = seconds - 1
+
+        console.log(minTimer, secondsAtTimer)
+    }
+
+
+
+
+    return `Temos aqui ${min} minutos e ${seconds} segundos, ja no while temos que ${minTimer} e em segundos seria ${secondsAtTimer}`
+}
+
+
+
+
+
+
+
+console.log(stopwatch(minutes))
+
+
+
+
