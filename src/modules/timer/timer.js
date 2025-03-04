66d0136db2df876398ad643d7c64f5ae4342aeff
DieguitoNC <diegocatalann@gmail.com>
2025-03-04 12:38:04 -0300

    sistema do cronometro funcionando

--- src/modules/timer/timer.js
+++ src/modules/timer/timer.js
@@ -1,7 +1,9 @@
 // Fazer toda a regra do cronometro !
 
-const pickMinutes = document.getElementsByClassName(".minutes")
-const pickSeconds = document.getElementsByClassName(".seconds")
+const pickMinutes = document.querySelector(".minutes")
+const pickSeconds = document.querySelector(".seconds")
+
+const audio = new  Audio("../../assets/bedside-clock-alarm-95792.mp3")
 console.log(pickMinutes) // tirar dps
 console.log(pickSeconds) // tirar dps
 
@@ -17,21 +19,19 @@ async function stopwatch(minutes) {
     let seconds = transformToSeconds(minutes)
     let min = minutes
 
-    while (seconds > 0) {
+    while (seconds >= 0) {
         let minTimer = Math.floor(seconds / 60)
         let secondsAtTimer = seconds % 60
 
         await new Promise((resolve) => {
-            setTimeout(resolve, 10) // valor tem que ser 1000
+            setTimeout(resolve, 100) // valor tem que ser 1000
 
-            pickMinutes.innerHTML = "minTimer"
-            pickSeconds.innerHTML = "secondsAtTimer"
+            pickMinutes.innerHTML = minTimer < 10 ? "0" + minTimer:minTimer
+            pickSeconds.innerHTML = secondsAtTimer < 10 ? "0" + secondsAtTimer:secondsAtTimer
         })
-
         seconds = seconds - 1
-        console.log({minTimer, secondsAtTimer})
     }
-
+    audio.play()
     console.log("Loop Finalizado !")
 }
 
